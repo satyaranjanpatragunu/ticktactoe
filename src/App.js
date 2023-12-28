20bc57d80634a7fae57c602ab4c968d5affd9908
satyaranjanpatragunu <rogergunu18@gmail.com>
2023-12-28 17:24:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Tictactoe from './component/tictactoe/Tictactoe';
+function App() {
+  return (
+    <div>
+     <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
